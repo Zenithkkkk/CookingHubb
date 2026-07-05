f78597aed4ce452774917d9a189b18215fc09c3e
Aylin <aylin.ma24@gmail.com>
2026-07-05 14:35:50 +0200

    multer pics size fix

--- config/multer.js
+++ config/multer.js
@@ -35,7 +35,10 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 5 * 1024 * 1024 }
+  limits: { 
+    fileSize: 5 * 1024 * 1024,
+    fieldSize: 10 * 1024 * 1024
+  }
 });
 
 module.exports = upload;
